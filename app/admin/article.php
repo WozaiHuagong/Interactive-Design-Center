587e80c69ba776d28a990603ac790077f750b2b6
435495971 <435495971@qq.com>
2017-08-03 00:00:28 +0800

    分类修复

--- app/admin/article.php
+++ app/admin/article.php
@@ -89,13 +89,6 @@ class article extends AWS_ADMIN_CONTROLLER
 			$where[] = 'comments <= ' . intval($_GET['comment_count_max']);
 		}
 
-		if ($articles_list = $this->model('article')->fetch_page('article', implode(' AND ', $where), 'id DESC', $_GET['page'], $this->per_page))
-		{
-			$search_articles_total = $this->model('article')->found_rows();
-		}
-
-		$articles_list = $this->articles_list_modify($articles_list);
-
 		$url_param = array();
 
 		foreach($_GET as $key => $val)
@@ -106,17 +99,33 @@ class article extends AWS_ADMIN_CONTROLLER
 			}
 		}
 
-		TPL::assign('pagination', AWS_APP::pagination()->initialize(array(
-			'base_url' => get_js_url('/admin/article/list/') . implode('__', $url_param),
-			'total_rows' => $search_articles_total,
-			'per_page' => $this->per_page,
-			'query_string_segment' => 'tmp'
-		))->create_links());
+
+		// $industryInfo_where=$where;
+		// $industryInfo_topic_id = $this->model('topic')->get_topic_by_title('行业资讯')["topic_id"];
+		// $industryInfo_where[] = ' category_id = ' .$industryInfo_topic_id;
+
+		// if ($industryInfo_list = $this->model('article')->fetch_page('article', implode(' AND ', $industryInfo_where), 'id DESC', $_GET['industryInfo_page'], $this->per_page)){
+		// 	$search_industryInfo_total = $this->model('article')->found_rows();
+		// }
+
+		// $industryInfo_list = $this->articles_list_modify($industryInfo_list);
+
+		// TPL::assign('industryInfo_pagination', AWS_APP::pagination()->initialize(array(
+		// 	'base_url' => get_js_url('/admin/article/list/') . implode('__', $url_param),
+		// 	'total_rows' => $search_industryInfo_total,
+		// 	'per_page' => $this->per_page,
+		// 	'query_string_segment' => 'industryInfo_page'
+		// ))->create_links());
+		$this->pagination_wrapper('行业资讯','industryInfo',$where,$url_param);
+		$this->pagination_wrapper('研究中心资讯','libInfo',$where,$url_param);
+		$this->pagination_wrapper('在研项目','working',$where,$url_param);
+		$this->pagination_wrapper('研究成果','finished',$where,$url_param);
+		
 
 		$this->crumb(AWS_APP::lang()->_t('文章管理'), 'admin/article/list/');
 
-		TPL::assign('articles_count', $search_articles_total);
-		TPL::assign('list', $articles_list);
+		//TPL::assign('articles_count', $search_articles_total);
+		TPL::assign('list', $industryInfo_list);
 		TPL::assign('category_list',$this->model('topic')->get_topic_list('type = "article"'));
 
 		TPL::output('admin/article/list');
@@ -153,5 +162,24 @@ class article extends AWS_ADMIN_CONTROLLER
 			}
 		}
 		return $articles_list;
+	}
+
+	private function pagination_wrapper($topic_title,$prefix,$where,$url_param){
+		$__where=$where;
+		$__topic_id = $this->model('topic')->get_topic_by_title($topic_title)["topic_id"];
+		$__where[] = ' category_id = ' .$__topic_id;
+
+		if ($__list = $this->model('article')->fetch_page('article', implode(' AND ', $__where), 'id DESC', $_GET[$prefix.'_page'], $this->per_page)){
+			$search__total = $this->model('article')->found_rows();
+		}
+
+		$__list = $this->articles_list_modify($__list);
+		TPL::assign($prefix.'_pagination', AWS_APP::pagination()->initialize(array(
+			'base_url' => get_js_url('/admin/article/list/') . implode('__', $url_param),
+			'total_rows' => $search__total,
+			'per_page' => $this->per_page,
+			'query_string_segment' => $prefix.'_page'
+		))->create_links());
+		TPL::assign($prefix.'_list', $__list);
 	}
 }
